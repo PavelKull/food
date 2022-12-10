5406b6111f0e74fdd278385756b3b19fdb3c2691
PavelK <pavelkulikov-s@mail.ru>
2022-12-10 18:35:09 +0300

    fixed timer

--- js/script.js
+++ js/script.js
@@ -36,14 +36,23 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //TIMER
 
-    const deadline = '2022-12-12';
+    const deadline = '2023-12-12';
 
     function getTimeRamaining(endtime) {
-        const t = Date.parse(endtime) - Date.parse(new Date()),
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endtime) - Date.parse(new Date());
+
+        if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
             days = Math.floor(t / (1000 * 60 * 60 * 24)),
             hours = Math.floor((t / (1000 * 60 * 60 * 60) % 24)),
             minutes = Math.floor((t / 1000 / 60) % 60),
             seconds = Math.floor((t / 1000) % 60);
+        }
         
         return {
             'total': t,
